e539ed3ab4290dc2d6ac90efc9aad5c368453c59
Zhou Yiming <jxzym25@gmail.com>
2014-03-09 13:47:54 +0800

    Merge

---
+++ Unity Project/Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : TouchLogic {
+
+	public Transform camera;
+	public Transform center;
+	public float hoverForce = 100.0f;
+	private Vector3 direction;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	public override void OnTouchBegan()
+	{
+		//need to cache the touch index that started on the pad so others wont interfere
+		direction = center.transform.position - camera.transform.position;
+		RaycastHit hit;
+		if (Physics.Raycast (camera.transform.position, direction, out hit)) {
+			float distanceToGround = hit.distance;
+			print(hit.collider.gameObject.tag);
+			hit.collider.gameObject.rigidbody.AddForce(direction * hoverForce, ForceMode.Acceleration);
+		}
+				
+	}
+}
+
