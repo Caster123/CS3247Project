a199a3298fa4671bf4dccc0e52e4cec838e61f55
Caster123 <magic.ken23@gmail.com>
2014-03-15 19:11:58 +0800

    Minor

---
+++ Unity Project/Assets/Scripts/HitIntro1.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitIntro1 : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        //this.gameObject.rigidbody.isKinematic = false;
+        //transform.Rotate(3f, 30f, 0);
+        //Destroy(this.gameObject);
+        //Application.LoadLevel("basic");
+        //Application.LoadLevel("1st");
+        StartCoroutine(Wait(0.1F));
+        //Application.LoadLevel("basic");
+    }
+
+    IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Application.LoadLevel("ManIntroduction2");
+    }    
+}
