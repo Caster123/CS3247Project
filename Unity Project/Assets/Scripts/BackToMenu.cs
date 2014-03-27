679213a692fd94c3c61c759976c20ddc6b5d756a
jxzym25 <jxzym25@gmail.com>
2014-03-27 20:14:51 +0800

    BOX

--- Unity Project/Assets/Scripts/BackToMenu.cs
+++ Unity Project/Assets/Scripts/BackToMenu.cs
@@ -5,7 +5,8 @@ public class BackToMenu : MonoBehaviour {
 	public string sceneName = "Main Menu";
 	// Use this for initialization
 	void Start () {
-		
+		if (SharedBehaviour.current.currentLevel == 0)
+			sceneName = "Main Menu";
 	}
 	
 	// Update is called once per frame
